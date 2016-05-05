7520b3cde4504e8009747708b218aa0fb95bd1c4
lucas95123 <1245554637@qq.com>
2016-05-06 00:53:33 +0800

    Some trail on router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
+router.get('/birds', function(req, res) {
+    res.send('Birds home page');
+});
+
 module.exports = router;
