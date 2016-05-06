66a6a15e53f1818e7f66875010729989c4eac397
lucas95123 <lucas95123@outlook.com>
2016-05-06 22:54:17 +0800

    created post response on index

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var searchManager = require('./searchManager');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { tabChoose:0 });
+  res.render('index', { tabChoose:0 , data:'丁诗伦大酒店'});
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('index', { tabChoose:0 });
+  searchManager.query('select * from HotelInfo', function (qerr, rows, fields) {
+    res.render('index', { tabChoose:0, data:rows[0].Province})
+  })
+});
+
+router.post('/index', function(req, res, next){
+  console.log(req.body);
+  res.render('index', { tabChoose:0, data:'提交成功'})
 });
 
 router.get('/comment',function(req,res,next){
   res.render('comment',{tabChoose:2});
 });
-
 
 router.get('/search',function(req,res,next){
   res.render('searchHotel',{tabChoose:1});
@@ -22,6 +29,5 @@ router.get('/search',function(req,res,next){
 router.get('/test',function(req,res,next){
   res.render('a2',{tabChoose:2})
 })
-
 
 module.exports = router;
