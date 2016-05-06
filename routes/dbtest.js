01fa5b93fe2e6e67678f50693988aef740b4fcd8
hzfmacbook <huangzf@MacBook-Pro.local>
2016-05-06 22:41:41 +0800

    add database

---
+++ routes/dbtest.js
@@ -0,0 +1,8 @@
+var filter=require('./filterManager');
+var result_result=function(result){
+  //result=result;
+  console.log(result);
+}
+
+filter.serch_hotel_info(null,null,null,null,null,null,null,null,"Hotel_Name",result_result);
+filter.serch_hotel_info(null,null,null,null,null,null,null,null,"Hotel_Name",result_result);
