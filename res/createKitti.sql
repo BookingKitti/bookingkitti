9d237f218f77dfa35311d7e1b643a5838531bfc7
lucas95123 <lucas95123@outlook.com>
2016-05-06 11:30:18 +0800

    Added mysql query to create databases

---
+++ res/createKitti.sql
@@ -0,0 +1,49 @@
+create table TicketsInfo(
+  AirTicket_ID int primary key auto_increment,
+  Departure varchar(50),
+  Airport varchar(50),
+  Destination varchar(50),
+  Depart_time datetime,
+  Arrive_time datetime,
+  Total int,
+  Available int,
+  Price int);
+
+create table UserAccount(
+  AccountID int primary key auto_increment);
+
+create table TicketsComments(
+  AirTickets_ID int,
+  Score float,
+  Account_ID int,
+  Comments text,
+  foreign key(Account_ID) references UserAccount(AccountID));
+
+create table HotelInfo(
+  Hotel_ID int primary key auto_increment,
+  Hotel_Name varchar(20),
+  Province varchar(20),
+  City varchar(20),
+  Address varchar(20),
+  Stars int,
+  Description text,
+  PhoneNumber varchar(20)
+);
+
+create table RoomInfo(
+  Hotel_ID int,
+  Type char(10),
+  Price float,
+  Available int,
+  Details text,
+  Total int,
+  foreign key(Hotel_ID) references HotelInfo(Hotel_ID)
+);
+
+create table HotelComments(
+  Hotel_ID int,
+  Scores float,
+  Account_ID int,
+  Comments text,
+  foreign key(Account_ID) references UserAccount(AccountID));
+);
