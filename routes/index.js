75d255b0a305bbd95f04783fcb336de33bd1e1cd
lucas95123 <lucas95123@outlook.com>
2016-05-07 09:59:24 +0800

    Added some comments

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var searchManager = require('./searchManager');
+var filterManager =
 
-/* GET home page. */
+/*@brief GET home page
+*render index.ejs
+*/
 router.get('/', function(req, res, next) {
   res.render('index', { tabChoose:0 , data:'丁诗伦大酒店'});
 });
 
+/*@brief GET index page
+*render index.ejs
+*by default select all the Hotel info
+*/
 router.get('/index', function(req, res, next) {
   searchManager.query('select * from HotelInfo', function (qerr, rows, fields) {
     res.render('index', { tabChoose:0, data:rows[0].Province})
@@ -18,10 +25,16 @@ router.post('/index', function(req, res, next){
   res.render('index', { tabChoose:0, data:'提交成功'})
 });
 
+/*@brief GET comment page
+*render comment.ejs
+*/
 router.get('/comment',function(req,res,next){
   res.render('comment',{tabChoose:2});
 });
 
+/*@brief GET search page
+*render search.ejs
+*/
 router.get('/search',function(req,res,next){
   res.render('searchHotel',{tabChoose:1});
 });
