7d22f6d5bc8fba64bfde5da843acf6bc322d1188
lucas95123 <lucas95123@outlook.com>
2016-05-10 10:26:20 +0800

    First commit on branch develop

--- routes/adminManager.js
+++ routes/adminManager.js
@@ -1,18 +1,18 @@
 //adminManager.js
-var searchManager=require('./searchManager')
+var searchManager = require('./searchManager')
 
-exports.add_hotel_info(Hotel_Name, Province, City, Address, Stars, Description, PhoneNumber, callback){
-  
-}
-
-exports.upload_hotel_photo(){
+exports.add_hotel_info(Hotel_Name, Province, City, Address, Stars, Description, PhoneNumber, callback) {
 
 }
 
-exports.delete_hotel_info(){
+exports.upload_hotel_photo(Hotel_ID, callback) {
 
 }
 
-exports.update_hotel_info(){
+exports.delete_hotel_info(Hotel_ID, callback) {
+
+}
+
+exports.update_hotel_info(Hotel_ID, Hotel_Name, Province, City, Address, Stars, Description, PhoneNumber, callback) {
 
 }
