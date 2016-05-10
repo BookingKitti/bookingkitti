be2129360d30d9efd5f1a8bb51e9fefc8b574920
Turtle-duck <turtleduck@126.com>
2016-05-10 09:14:34 -0700

    change booking

--- routes/dbtest.js
+++ routes/dbtest.js
@@ -1,8 +1,17 @@
 var filter=require('./filterManager');
-var result_result=function(result){
+var book=require('./bookingManager');
+var result_result=function(qerr, vals, fields){
   //result=result;
-  console.log("delete");
+  for(var index in vals){
+     var tuple=vals[index];
+     var result='';
+     for(var attribute in tuple){
+       result+=tuple[attribute];
+     }
+     console.log(result);
+   }
 }
 
 filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result_result);
 filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result_result);
+book.change_room_data(1, "HIGH_ROOM", "2016/5/20", result_result);
