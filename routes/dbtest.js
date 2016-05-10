1a2719a83fc6ad6cf72e4f7794aaf91217372be7
hzfmacbook <huangzf@MacBook-Pro.local>
2016-05-11 00:12:33 +0800

    fix select ... and ...'s bug

--- routes/dbtest.js
+++ routes/dbtest.js
@@ -1,8 +1,23 @@
-var filter=require('./filterManager');
-var result_result=function(result){
-  //result=result;
-  console.log("delete");
-}
+//var filter=require('./filterManager');
 
-filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result_result);
-filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result_result);
+var result2log=function(qerr,vals,fields){
+  for(var index in vals){
+    var tuple=vals[index];
+    var result='';
+    for(var attribute in tuple){
+      result+=(tuple[attribute] + " ");
+    }
+    console.log(result);
+  }
+};
+
+var str = "%";
+var con = "str";
+for (var i = 0; i < con.length; i++) {
+    str += con[i] + "%";
+}
+console.log(con.length);
+
+//exports.search_airticket_info = function(departure, airport, destination, depart_time, arrive_time, l_price, h_price, sort_attr, 1, callback)
+//filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result2log);
+//filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result2log);
