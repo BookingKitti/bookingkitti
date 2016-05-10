cffcf8b7b0fc2359942b1c5d958a51322aa261c5
hzfmacbook <huangzf@MacBook-Pro.local>
2016-05-11 00:52:44 +0800

    add test js

--- routes/dbtest.js
+++ routes/dbtest.js
@@ -1,4 +1,4 @@
-//var filter=require('./filterManager');
+var filter=require('./filterManager');
 
 var result2log=function(qerr,vals,fields){
   for(var index in vals){
@@ -11,13 +11,8 @@ var result2log=function(qerr,vals,fields){
   }
 };
 
-var str = "%";
-var con = "str";
-for (var i = 0; i < con.length; i++) {
-    str += con[i] + "%";
-}
-console.log(con.length);
 
+//hotel_name, province, city, addr, date_in, date_out, l_price, h_price, sort_attr, hotel_asc_flag, callback)
 //exports.search_airticket_info = function(departure, airport, destination, depart_time, arrive_time, l_price, h_price, sort_attr, 1, callback)
-//filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result2log);
+filter.search_hotel_info('杭州法云安缦酒店', '浙江', '杭州', '西湖', '2016/05/09', '2016/05/10', 4835.0, 7000.0, null, null, result2log);
 //filter.search_airticket_info(null,null,null,null,null,null,null,"Price",result2log);
