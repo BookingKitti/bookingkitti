b53fb1888e095b0cb9396da676d7f955468d4814
dingshilun <zgrxdhp@qq.com>
2016-05-17 11:31:21 +0800

    fucking debugged NEW UI

--- routes/searchManager.js
+++ routes/searchManager.js
@@ -3,14 +3,8 @@ var mysql = require("mysql");
 var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: '', //'lucas95123',
     database: 'kitty',
-=======
-    password: 'lucas95123', //'lucas95123',
-    database: 'kitty',
-
->>>>>>> c0ab79899fb568caffee47810329e28fc1bae8d7
 });
 
 exports.query = function(sql, callback, req, res) {
