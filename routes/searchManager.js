d6d368f5fd5196210a8adbf5e23a5777a7733e5b
Turtle-duck <turtleduck@126.com>
2016-05-24 07:57:26 -0700

    c

--- routes/searchManager.js
+++ routes/searchManager.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '', //'lucas95123',
+    password: 'yahan520', //'lucas95123',
     database: 'kitty',
 });
 
