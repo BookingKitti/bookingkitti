fecf52e148c667e839e5c0a3d20be9f5e2d6694a
hzfGuardian <412571747@qq.com>
2016-05-25 16:53:42 +0800

    蛤蛤蛤

--- routes/test.js
+++ routes/test.js
@@ -1,3 +1,27 @@
-var comments=require('./comment');
+//var comments=require('./comment');
 
-comments.insert("1","1","1","fuck!!!");
+//comments.insert("1","1","1","fuck!!!");
+function addDate(date, days){
+    var d=new Date(date);
+    d.setDate(d.getDate()+days);
+    var month=d.getMonth()+1;
+    var day = d.getDate();
+    if(month<10){
+        month = "0"+month;
+    }
+    if(day<10){
+        day = "0"+day;
+    }
+    var val = d.getFullYear()+"-"+month+"-"+day;
+    return val;
+}
+
+var start = '2016-06-05';
+var end = '2016-06-08';
+var date = new Date(start);
+for (var i = start; i <= end; i = addDate(i, 1)) {
+    console.log(i);
+}
+//var myDate = new Date('2016-06-05');
+//myDate = myDate + 1;
+//console.log(myDate.toLocaleDateString());
