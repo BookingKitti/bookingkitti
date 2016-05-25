3efd283cb0c00c27dcbfb2429cdba95d4017da9d
lucas95123 <1245554637@qq.com>
2016-05-25 17:32:27 +0800

    Hello

--- routes/index.js
+++ routes/index.js
@@ -586,6 +586,7 @@ router.post('/updateHotel', function(req, res) {
 router.post('/uploadRoomPics', function(req, res) {
     adminManager.upload_room_photo(req, res, function(err, req, res) {
       // body...
+      console.log("Here jdiawjdi j");
       showAdminDetail(req, res, req.query.Hotel_ID);
     })
 })
