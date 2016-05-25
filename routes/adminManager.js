cfde319036e9e2e0c14da81b826d305677d6f5dd
hzfGuardian <412571747@qq.com>
2016-05-25 14:00:39 +0800

    delete room type

--- routes/adminManager.js
+++ routes/adminManager.js
@@ -427,12 +427,12 @@ exports.update_airticket_info = function(Airticket_ID, Departure, Airport, Desti
 
 exports.delete_room_type = function (Hotel_ID, Type, callback) {
 
-    var main_sql = "delete from RoomType where Hotel_ID = " + Hotel_ID + " and Type = " + Type;
+    var main_sql = "delete from RoomType where Hotel_ID = " + Hotel_ID + " and Type = '" + Type + "'";
 
     searchManager.query(main_sql, function(qerr) {
         console.log(qerr);
         if (!qerr) {
-            var clause_sql = "delete from RoomInfo where Hotel_ID = " + Hotel_ID + " and Type = " + Type;
+            var clause_sql = "delete from RoomInfo where Hotel_ID = " + Hotel_ID + " and Type = '" + Type + "'";
             searchManager.query(clause_sql, function(qerr) {
                 callback(qerr);
             }
@@ -443,6 +443,20 @@ exports.delete_room_type = function (Hotel_ID, Type, callback) {
     });
 }
 
-exports.update_room_info = function (Hotel_ID, Type, callback) {
-    
+/*
+* @Hotel_ID, Type, Room_date: conditions
+* @modify: Price
+*/
+
+exports.update_room_info = function (Hotel_ID, Type, Room_date, Price, callback) {
+
+    if (Price != null) {
+        var sql = "update RoomInfo set Price = " + Price
+        + " where Hotel_ID = " + Hotel_ID + " and Type = '" + Type + "' and Room_date = '" + Room_date + "'";
+
+        searchManager.query(sql, function(qerr) {
+            callback(qerr);
+        }
+    }
+
 }
